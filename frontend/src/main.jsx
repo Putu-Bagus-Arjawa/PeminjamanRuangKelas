6630959df39e22d705dd01d09b2da4603bfd7f0f
Satya Adnyana <satya.surf@gmail.com>
2025-06-22 22:22:39 +0800

    login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,12 @@ import './index.css'
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Register from './pages/Register.jsx'
+import Login from './pages/Login.jsx'
 import App from './App.jsx'
 
 const routes = createBrowserRouter([
   {path: '/register', element: <Register/>},
-  {path: '/login', element: <Register/>},
+  {path: '/login', element: <Login/>},
   {path: '*', element: <App/>},
 ])
 
